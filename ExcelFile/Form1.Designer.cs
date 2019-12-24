5d641c2233cd95efad91e4504aa3f22715b3b73d
SERGSIRKO1980 <sergsirko1980@yahoo.com.ua>
2019-12-24 11:54:17 +0200

    Add project files.

---
+++ ExcelFile/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ExcelFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(351, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 84);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(259, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 315);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ExcelFile/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Xml;
+using System.Reflection;
+using System.Threading;
+
+namespace ExcelFile
+{
+    public partial class Form1 : Form
+    {
+        Excel.Application xlap = new Excel.Application();
+         
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            if (xlap == null)
+            {
+                label1.Text = "Excel Library is not installed ";
+                label1.ForeColor = Color.Red;
+            }
+            else
+            {
+                label1.Text = "Excel Library is installed ";
+                label1.ForeColor = Color.Green;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            ProcessStartInfo procInfo = new ProcessStartInfo();
+
+            procInfo.Arguments = "https://www.bowok.pp.ua/sitemap.xml?page=1";
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(procInfo.Arguments);
+
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = Missing.Value;
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+
+         
+            xlWorkSheet.Cells[1, 1] = "Ref";
+            xlWorkSheet.Cells[1, 2] = "Date";
+            int number = 2;
+            // обход всех узлов в корневом элементе
+            foreach (XmlNode xnode in xRoot)
+            {
+               
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    
+                   
+                    // если узел - loc
+                    if (childnode.Name == "loc")
+                    {
+                       
+                        xlWorkSheet.Cells[number, 1] = '"' + childnode.InnerText + '"';
+                    }
+                    //  если узел loc
+                    if (childnode.Name == "lastmod")
+                    {
+                        
+                        xlWorkSheet.Cells[number, 2] = '"' + childnode.InnerText + '"';
+                    }
+                    
+                }
+                number++;
+            }
+
+
+            xlWorkBook.SaveAs(@"C:\Users\user\Desktop\parsing.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+        }
+    }
+}
